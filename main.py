b21083bd210db4421240c9e671e4fb406008aab2
garinegor <garinegor@gmail.com>
2019-07-04 16:24:38 +0300

    added ssid input instead of dropdown

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ ssids = ["Home", "F808", "Kek"]
 def hello():
 	ssids = [network["ssid"] for network in wifi.scan_networks("wlan0")]
 	return render_template("wifi.html", ssids=ssids)
+
+
+@app.route('/save_network', methods=['POST'])
+def save_network(request):
+	wifi.add_network("wlan0", {"ssid": request.ssid, "psk": request.psk})
+
