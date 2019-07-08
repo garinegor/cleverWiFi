ffb4c37cae9e1f90f36fa052b44a4fe9fa1747bf
Egor Garin <garinegor@gmail.com>
2019-07-08 14:37:22 +0100

    updated wifi.py

--- wifi.py
+++ wifi.py
@@ -19,6 +19,118 @@ logging.basicConfig(level=logging.DEBUG,
                     filemode='w')
 
 
+def add_network(_iface, _parameters):
+	"""
+	Add new network with passed parameters to the end of the networks list. Disables all other networks
+	"""
+	id = run_program("wpa_cli -i %s add_network")[:-1]
+	if id.isdigit():
+		for parameter in _parameters:
+			_set_parameter(_iface, id, parameter, _parameters[parameter])
+		enable_network(id)
+		return id
+	else:
+		logging.error("Unable to add new network.")
+		return None
+
+
+def select_network(_iface, _id):
+	"""
+	Set the highest prioriry to the network with passed id
+	"""
+	r = run_program("wpa_cli -i %s list_networks" % _iface)
+	ids = list(map(int, [i.split()[0] for i in r.split("\n")[1: -1]]))
+	priorities = list(map(int, [_get_parameter(_iface, id, "priority") for id in ids]))
+	present_index = ids.index(id)
+	target_index = priorities.index(max(priorities))
+	if target_index != present_index:
+		_set_parameter(_iface, ids[present_index], "priority", priorities[target_index])
+		_set_parameter(_iface, ids[target_index], "priority", priorities[present_index])
+
+
+def scan_networks(_iface, retry=10):
+	"""
+	Grab a list of wireless networks within range, and return a list of dicts describing them.
+	"""
+	while retry > 0:
+		if "OK" in run_program("wpa_cli -i %s scan" % _iface):
+			networks = []
+			r = run_program("wpa_cli -i %s scan_result" % _iface).strip()
+			if "bssid" in r and len(r.split("\n")) > 1:
+				for line in r.split("\n")[1:]:
+					b, fr, s, f = line.split()[:4]
+					ss = " ".join(line.split()[4:])  # Hmm, dirty
+					networks.append({"bssid": b, "freq": fr, "sig": s, "ssid": ss, "flag": f})
+				return networks
+		retry -= 1
+		logging.debug("Couldn't retrieve networks, retrying")
+		time.sleep(0.5)
+	logging.error("Failed to list networks")
+
+
+def get_saved_networks(_iface):
+	"""
+	Get list with networks saved in wpa_supplicant
+	"""
+	r = run_program("wpa_cli -i %s list_networks" % _iface)
+	networks = []
+	if len(r.split("\n")) > 1:
+		for network in r.split("\n")[1:-1]:
+			parameters = network.split()
+			nid = int(parameters[0])
+			networks.append(dict(zip(["id", "ssid", "mode"], [nid, parameters[1], int(_get_parameter(_iface, nid, "mode"))])))
+	return networks
+
+
+def switch_mode(_mode):
+	"""
+	Configure static IP and services
+	"""
+	if _mode == "cli":
+		run_program("")
+		run_program("")
+		run_program("")
+		run_program("")
+		run_program("")
+		run_program("")
+	elif _mode == "ap":
+		with open("/etc/dhcpcd.conf", "a") as f:
+			f.write("")
+		run_program("")
+		run_program("")
+		run_program("")
+		run_program("")
+		run_program("")
+
+
+def enable_network(_iface, _id):
+	"""
+	Enable network with passed id
+	"""
+	run_program("wpa_cli -i %s enable_network %d" % (_iface,  _id))
+
+
+def disable_network(_iface, _id):
+	"""
+	Disable network with passed id
+	"""
+	run_program("wpa_cli -i %s disable_network %d" % (_iface,  _id))
+
+
+def _get_parameter(_iface, _id, _name):
+	"""
+	Get network parameter value
+	"""
+	return run_program("wpa_cli -i %s get_network %d %s" % (_iface, _id, _name))
+
+
+def _set_parameter(_iface, _id, _name, _value):
+	"""
+	Set network parameter value
+	"""
+	return run_program("wpa_cli -i %s set_network %d %s %s" % (_iface, _id, _name, _value))
+
+
 def run_program(rcmd):
 	"""
 	Runs a program, and it's paramters (e.g. rcmd="ls -lh /var/www")
@@ -70,89 +182,3 @@ def get_wnics():
 			ifaces.append(line.split()[0])
 	return ifaces
 
-
-def scan_networks(iface, retry=10):
-	"""
-	Grab a list of wireless networks within range, and return a list of dicts describing them.
-	"""
-	while retry > 0:
-		if "OK" in run_program("wpa_cli -i %s scan" % iface):
-			networks = []
-			r = run_program("wpa_cli -i %s scan_result" % iface).strip()
-			if "bssid" in r and len(r.split("\n")) > 1:
-				for line in r.split("\n")[1:]:
-					b, fr, s, f = line.split()[:4]
-					ss = " ".join(line.split()[4:])  # Hmm, dirty
-					networks.append({"bssid": b, "freq": fr, "sig": s, "ssid": ss, "flag": f})
-				return networks
-		retry -= 1
-		logging.debug("Couldn't retrieve networks, retrying")
-		time.sleep(0.5)
-	logging.error("Failed to list networks")
-
-
-def enable_network(_iface, _id):
-	"""
-	Enable network with passed id
-	"""
-	run_program("wpa_cli -i %s enable_network %d" % (_iface,  _id))
-
-
-def disable_network(_iface, _id):
-	"""
-	Disable network with passed id
-	"""
-	run_program("wpa_cli -i %s disable_network %d" % (_iface,  _id))
-
-
-def add_network(_iface, _parameters):
-	"""
-	Add new network with passed parameters to the end of the networks list. Disables all other networks
-	"""
-	id = run_program("wpa_cli -i %s add_network")[:-1]
-	if id.isdigit():
-		for parameter in _parameters:
-			_set_parameter(_iface, id, parameter, _parameters[parameter])
-		enable_network(id)
-		return id
-	else:
-		logging.error("Unable to add new network.")
-		return None
-
-
-def select_network(_iface, _id):
-	"""
-	Set the highest prioriry to the network with passed id
-	"""
-	r = run_program("wpa_cli -i %s list_networks" % _iface)
-	ids = list(map(int, [i.split()[0] for i in r.split("\n")[1: -1]]))
-	priorities = list(map(int, [_get_parameter(_iface, id, "priority") for id in ids]))
-	present_index = ids.index(id)
-	target_index = priorities.index(max(priorities))
-	if target_index != present_index:
-		_set_parameter(_iface, ids[present_index], "priority", priorities[target_index])
-		_set_parameter(_iface, ids[target_index], "priority", priorities[present_index])
-
-
-def _get_parameter(_iface, _id, _name):
-	"""
-	Get network parameter value
-	"""
-	return run_program("wpa_cli -i %s get_network %d %s" % (_iface, _id, _name))
-
-
-def _set_parameter(_iface, _id, _name, _value):
-	"""
-	Set network parameter value
-	"""
-	return run_program("wpa_cli -i %s set_network %d %s %s" % (_iface, _id, _name, _value))
-
-
-# def disable_all(_iface, _id):
-# 	"""
-# 	Disable wireless network by id.
-# 	"""
-# 	lines = run_program("wpa_cli -i %s list_networks" % _iface).split("\n")
-# 	if lines:
-# 		for line in lines[1:-1]:
-# 			run_program("wpa_cli -i %s disable_network %s" % (_iface, line.split()[0]))
