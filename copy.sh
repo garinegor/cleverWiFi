8410b5902718007b08654d68e6fede456196fdaa
Egor Garin <garinegor@gmail.com>
2019-07-09 13:19:20 +0100

    added static file copy script

---
+++ copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp front/wifi.html ~/catkin_ws/src/clever/clever/www
+cp front/index.html ~/catkin_ws/src/clever/clever/www
+cp front/js/wifi.js ~/catkin_ws/src/clever/clever/www/js
+cp front/js/jquery-3.4.1.min.js ~/catkin_ws/src/clever/clever/www/js
+cp front/js/socket.io.js ~/catkin_ws/src/clever/clever/www/js
+echo "Done"
