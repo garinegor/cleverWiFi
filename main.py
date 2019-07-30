3c4d382814662ec2d3e1bea5c81673c2ab94ca11
Igor Nurullaev <mighty.igor@yandex.ru>
2019-07-31 01:27:08 +0300

    Some web upgrades

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import wifi
+# import wifi
 from flask import Flask, render_template, request, redirect, jsonify
 
 app = Flask(__name__)
 
-iface = wifi.get_wnics()[0]
+# iface = wifi.get_wnics()[0]
 ssids = ["Home", "F808", "Kek"]
 
 wrap = lambda s: '\'"' + s + '"\''
@@ -32,6 +32,7 @@ def settings():
     wifi_data = {
         "pass": "somewifipassword",
         "own_name": "CLEVERWEB",
+        "is_ap": False
     }
 
     return render_template("settings.html", wifi=wifi_data, navbar_title='settings', settings=settings_menu)
@@ -39,13 +40,13 @@ def settings():
 
 @app.route("/wifi/available")
 def available():
-    ssids = [network["ssid"] for network in wifi.scan_networks("wlan0")]
+    # ssids = [network["ssid"] for network in wifi.scan_networks("wlan0")]
     return jsonify({"available": ssids})
 
 
 @app.route('/save_network', methods=['POST'])
 def save_network():
-    wifi.add_network(iface, {"ssid": wrap(request.form["ssid"]), "psk": wrap(request.form["psk"])})
+    # wifi.add_network(iface, {"ssid": wrap(request.form["ssid"]), "psk": wrap(request.form["psk"])})
     return redirect("/")
 
 
